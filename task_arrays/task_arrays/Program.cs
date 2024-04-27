3026f27a7c43ce23ff3562a614e461cd071603e5
DannyNepo <147526132+DannyNepo@users.noreply.github.com>
2024-04-27 21:44:27 +0700

    init

---
+++ task_arrays/task_arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Random rndm = new Random();
+
+        Console.WriteLine("введите количество строк двумерного массива: ");
+        int lines = int.Parse(Console.ReadLine());
+        Console.WriteLine("введите количество столбцов двумерного массива: ");
+        int columns = int.Parse(Console.ReadLine());
+
+        int[,] array = new int[lines, columns];
+        int sum = 0;
+
+        for (int i = 0; i < lines; i++) 
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                array[i, j] = rndm.Next(0, 10);
+                Console.Write($"{array[i, j]}   ");
+                sum += array[i, j];
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine($"сумма всех элементов массива: {sum}");
+    }
+}
---
+++ task_arrays/task_arrays_2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace task_arrays_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndm = new Random();
+
+            Console.WriteLine("введите количество строк двумерного массива: ");
+            int lines = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите количество столбцов двумерного массива: ");
+            int columns = int.Parse(Console.ReadLine());
+
+            int[,] arrayA = new int[lines, columns];
+            int[,] arrayB = new int[lines, columns];
+            int[,] arrayC = new int[lines, columns];
+
+            Console.WriteLine("матрица А: ");
+
+            for(int i = 0; i < lines; i++) 
+            {
+                for(int j = 0; j < columns; j++)
+                {
+                    arrayA[i, j] = rndm.Next(0, 10);
+                    Console.Write($"{arrayA[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("матрица B: ");
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arrayB[i, j] = rndm.Next(0, 10);
+                    Console.Write($"{arrayB[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("сумма матриц А и В: ");
+
+            for (int i = 0; i < lines; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    arrayC[i, j] = arrayA[i, j] + arrayB[i, j];
+                    Console.Write($"{arrayC[i, j]}  ");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
